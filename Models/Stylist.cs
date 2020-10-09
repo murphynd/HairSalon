ba93f81e0580af4f719cc8317a4b820dbfb58c68
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-10-09 09:48:21 -0700

    build out of Stylist

---
+++ Models/Stylist.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace Salon.Models
+{
+  public class Stylist
+  {
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Details { get; set; }
+
+  }
+}
