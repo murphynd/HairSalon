793c4a7c9539c2c7f2fe624fd7ad6aad1f4a64e2
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-10-15 13:36:34 -0700

    removes using system.threading.Tasks from Client controller

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Threading.Tasks;
 using System;
 namespace Salon.Controllers
 {
