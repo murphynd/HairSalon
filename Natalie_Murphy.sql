4739842ee18cff5257e1ea7e587468161c0d8229
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-10-20 19:36:45 -0700

    .sql file updated

--- Natalie_Murphy.sql
+++ Natalie_Murphy.sql
@@ -1,34 +1,62 @@
-CREATE DATABASE `Natalie_Murphy` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `Natalie_Murphy`;
-CREATE TABLE `Clients`
-(
-  `ClientID` int
-(11) NOT NULL AUTO_INCREMENT,
-  `Description` varchar
-(450) DEFAULT NULL,
-  `Address` varchar
-(450) DEFAULT NULL,
-  `StylistID` int
-(11) DEFAULT '0',
-  `City` varchar
-(200) DEFAULT NULL,
-  `State` varchar
-(3) DEFAULT NULL,
-  `Email` varchar
-(200) DEFAULT NULL,
-  PRIMARY KEY
-(`ClientID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `Stylists`
-(
-  `StylistId` int
-(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar
-(450) DEFAULT NULL,
-  `Details` varchar
-(450) DEFAULT NULL,
-  `Image` varchar
-(2000) DEFAULT NULL,
-  PRIMARY KEY
-(`StylistId`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: Natalie_Murphy
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Clients`
+--
+
+DROP TABLE IF EXISTS `Clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `Clients` (
+  `ClientID` int(11) NOT NULL AUTO_INCREMENT,
+  `Description` varchar(450) DEFAULT NULL,
+  `Address` varchar(450) DEFAULT NULL,
+  `StylistID` int(11) DEFAULT '0',
+  `City` varchar(200) DEFAULT NULL,
+  `State` varchar(3) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`ClientID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Stylists`
+--
+
+DROP TABLE IF EXISTS `Stylists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `Stylists` (
+  `StylistId` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(450) DEFAULT NULL,
+  `Details` varchar(450) DEFAULT NULL,
+  `Image` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`StylistId`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-20 19:35:13
